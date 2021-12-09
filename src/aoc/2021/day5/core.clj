2806832c48a29d4f82d66dc41be313d8ca83236d
Joaquín García Sastre <jgarciasastre@twilio.com>
2021-12-09 17:16:32 +0100

    Day5 solved!

--- src/aoc/2021/day5/core.clj
+++ src/aoc/2021/day5/core.clj
@@ -2,17 +2,19 @@
   (:require
    [aoc.2021.day5.input :as input]))
 
+(Math/abs -2)
+
 (defn- straight-line [[[x1 y1] [x2 y2]]]
   (or (= x1 x2) (= y1 y2)))
 
 (defn- add-line [board [[x1 y1] [x2 y2]]]
-  (loop [y_board board
-         y (min y1 y2)]
-    (if (> y (max y1 y2)) y_board
-        (let [updated_col (loop [x_board y_board
-                                 x (min x1 x2)]
-                            (if (> x (max x1 x2)) x_board (recur (update-in x_board [y x] inc) (+ x 1))))]
-          (recur updated_col (+ y 1))))))
+  (let [n (max (Math/abs (- x1 x2)) (Math/abs (- y1 y2)))
+        dx (cond (> x1 x2) -1 (> x2 x1) 1 :else 0)
+        dy (cond (> y1 y2) -1 (> y2 y1) 1 :else 0)]
+  (loop [b board
+         i 0]
+    (if (> i n) b
+      (recur (update-in b [(+ y1 (* dy i)) (+ x1 (* dx i))] inc) (+ i 1))))))
 
 (defn- count-danger [board]
   (loop [rows board
@@ -23,19 +25,33 @@
         (rest rows)
         (+ acc (count (filter #(> % 1) (first rows))))))))
 
-(defn- create-board [lines]
-  (let [filtered-lines (filter straight-line lines)
-        board (repeat (repeat 0))]
+(defn- max-coord [[p1 p2]]
+  (max (apply max p1) (apply max p2)))
+
+(defn- create-board [f lines]
+  (let [filtered-lines (filter f lines)
+        size (+ (apply max (map max-coord lines)) 1)
+        board (vec (repeat size (vec (repeat size 0))))]
     (loop [l filtered-lines
            b board]
       (if (empty? l)
         {:board b :danger (count-danger b)}
         (recur (rest l) (add-line b (first l)))))))
 
-;; Solve test input
-(create-board input/test-input)
+
+;; Solve test input part one
+(def solution-test-input-part-one (create-board straight-line input/test-input))
 
 ;; Solve part one
-(create-board input/puzzle-input)
+(def solution-part-one (create-board straight-line input/puzzle-input))
 
+(defn- also-diagonal [[[x1 y1] [x2 y2]]]
+  (or (= x1 x2) (= y1 y2) (= (max (- y1 y2) (- y2 y1)) (max (- x1 x2) (- x2 x1)))))
 
+;; Solve test input part two
+(def solution-test-input-part-one (create-board also-diagonal input/test-input))
+
+(create-board also-diagonal [[[0 0][5 5]] [[0 9][9 0]] [[9 9][0 0]]])
+
+;; Solve part two
+(def solution-part-one (create-board also-diagonal input/puzzle-input))
